f377ea8707c1039113026536ce5d42a65e84707d
Torii Haruki <tototo.haruki.1006@gmail.com>
2018-08-23 14:56:15 +0900

    1st commit

---
+++ coordinate.py
@@ -0,0 +1,25 @@
+print("Number of atoms ?")
+natom = input('')
+
+file = open("MolView.mol", "r")
+lines = file.readlines()
+file.close()
+
+file = open("atom_coordinate.dat", "w")
+
+data = []
+for i in range(4, 4+natom):
+    data.append(lines[i])
+
+coordinates = {}
+for i in range(0, natom):
+    temp = data[i].split()
+
+    coordinates["atom"] = "\'" + temp[3] + "\'"
+    coordinates["x"] = temp[0]
+    coordinates["y"] = temp[1]
+    coordinates["z"] = temp[2]
+
+    file.write(coordinates["atom"] +"\t"+ coordinates["x"] +"\t"+ coordinates["y"] +"\t"+ coordinates["z"] + "\n")
+
+file.close()
